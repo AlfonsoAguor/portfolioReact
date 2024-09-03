da307ad38896682f3ad401125918af6b3f3d5654
AlfonsoAguor <56021919+AlfonsoAguor@users.noreply.github.com>
2024-09-03 16:35:37 +0200

    Update Contact.jsx

--- cliente/src/componentes/Contact.jsx
+++ cliente/src/componentes/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
       e.preventDefault();
   
       try {
-        const response = await fetch('http://localhost:3001/send', {
+        const response = await fetch('https://portfolio-react-coral-eight.vercel.app/send', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
